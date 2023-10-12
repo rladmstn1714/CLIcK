1d24adcc40ce2d52b15645cf0637786cbe086773
Scott Suk <scottsuk0306@gmail.com>
2023-10-12 11:44:10 +0900

    Finalize Geography

---
+++ verify_data.py
@@ -0,0 +1,16 @@
+import os
+import json
+
+file_path = "/mnt/sda/juyoung/CLICK/Dataset/Culture/Korean Geography/Geography_KIIP_new.json"
+
+with open(file_path, "r", encoding="utf-8") as file:
+    data_list = json.load(file)
+    
+# print(data_list)
+print(len(data_list))
+
+ids = []
+for data in data_list:
+    ids.append(data['id'])
+
+assert(len(ids) == len(set(ids)))
