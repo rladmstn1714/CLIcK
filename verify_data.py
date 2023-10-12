f09a76464c712d993a16accaf514991582f96a70
Scott Suk <scottsuk0306@gmail.com>
2023-10-12 13:55:50 +0900

    Finalize History

--- verify_data.py
+++ verify_data.py
@@ -1,7 +1,7 @@
 import os
 import json
 
-file_path = "/mnt/sda/juyoung/CLICK/Dataset/Culture/Korean Geography/Geography_KIIP_new.json"
+file_path = "/mnt/sda/juyoung/CLICK/Dataset/Culture/Korean History/History_KHB_new.json"
 
 with open(file_path, "r", encoding="utf-8") as file:
     data_list = json.load(file)
@@ -13,4 +13,5 @@ ids = []
 for data in data_list:
     ids.append(data['id'])
 
+assert(len(ids) == len(data_list))
 assert(len(ids) == len(set(ids)))
