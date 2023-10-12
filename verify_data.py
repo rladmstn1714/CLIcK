1347b507246f93e2803cede380df70bc3841d5d6
Scott Suk <scottsuk0306@gmail.com>
2023-10-12 15:56:52 +0900

    Update Contextual & Functional

--- verify_data.py
+++ verify_data.py
@@ -1,11 +1,33 @@
 import os
 import json
 
-file_path = "/mnt/sda/juyoung/CLICK/Dataset/Language/Contextual/Contextual_TOPIK_new.json"
+file_path = "/mnt/sda/juyoung/CLICK/Dataset/Language/Functional/Functional_CSAT_new.json"
+# new_file_path = "/mnt/sda/juyoung/CLICK/Dataset/Language/Contextual/Contextual_TOPIK_new_new.json"
 
 with open(file_path, "r", encoding="utf-8") as file:
     data_list = json.load(file)
     
+    
+# for data in data_list:
+    
+#     context = data['question'].split("\n")[-1]
+#     if len(context) > 50:
+#         print(f"paragraph should be {context}, not {data['paragraph']}")
+#         data['paragraph'] = context.strip()
+#         data['question'] = data['question'].split("\n")[0].strip()
+    
+
+# # Open a file for writing with UTF-8 encoding
+# with open(new_file_path, 'w', encoding='utf-8') as file:
+#     # Iterate through each dictionary in the list
+#     for dictionary in data_list:
+#         # Convert the dictionary to a JSON string
+#         json_str = json.dumps(dictionary, ensure_ascii=False, indent=4)
+        
+#         # Write the JSON string to the file with a newline
+#         file.write(json_str + '\n')
+    
+
 # print(data_list)
 print(file_path.split("/")[-1])
 print(len(data_list))
